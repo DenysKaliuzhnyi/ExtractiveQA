324df75d9a1d6fc1bf5e1fa3df96ad876b6e1e57
DenysKaliuzhnyi <denyskaliuzhnyi@gmail.com>
2024-11-25 16:21:00 +0200

    model api: use archive, better logging

--- model_api.py
+++ model_api.py
@@ -3,35 +3,49 @@ from pydantic import BaseModel
 from transformers import pipeline, AutoModelForQuestionAnswering, AutoTokenizer
 from google.cloud import storage
 import os
+import tarfile
+import logging
 
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+logger = logging.getLogger(__name__)
 
 app = FastAPI()
 MODEL_BUCKET = os.getenv("MODEL_BUCKET")
-MODEL_PATH = os.getenv("MODEL_PATH")
+MODEL_FILE = 'model.tar.gz'
 MODEL_DIR = "artifacts/model"
 
-def download_folder(bucket_name, folder_name, destination_dir):
+
+def download_blob(bucket_name, file_name, destination_path):
+    if not os.path.exists(destination_path):
+        os.makedirs(destination_path)
+
     client = storage.Client()
     bucket = client.bucket(bucket_name)
-    blobs = bucket.list_blobs(prefix=folder_name)
+    blob = bucket.blob(file_name)
 
-    if not os.path.exists(destination_dir):
-        os.makedirs(destination_dir)
+    to_name = os.path.basename(file_name)
+    to_path = os.path.join(destination_path, to_name)
+    blob.download_to_filename(to_path)
+    logger.info(f"Downloaded {blob.name} to {to_path}")
 
-    for blob in blobs:
-        local_file_path = os.path.join(destination_dir, blob.name)
-        os.makedirs(os.path.dirname(local_file_path), exist_ok=True)
-        blob.download_to_filename(local_file_path)
-        print(f"Downloaded {blob.name} to {local_file_path}")
+    logger.info(f"Extracting archive ...")
+    path = os.path.join(destination_path, file_name)
+    with tarfile.open(path, "r:gz") as tar:
+        tar.extractall(path=destination_path)
 
 
 if not os.path.exists(MODEL_DIR):
-    print("Model not found locally. Downloading from GCS...")
-    download_folder(MODEL_BUCKET, MODEL_PATH, os.path.dirname(MODEL_DIR))
+    logger.info("Model not found locally. Downloading from GCS...")
+    download_blob(MODEL_BUCKET, MODEL_FILE, os.path.dirname(MODEL_DIR))
 
 model = AutoModelForQuestionAnswering.from_pretrained(MODEL_DIR)
 tokenizer = AutoTokenizer.from_pretrained(MODEL_DIR)
 qa_pipeline = pipeline("question-answering", model=model, tokenizer=tokenizer)
+
 
 class QARequest(BaseModel):
     context: str
